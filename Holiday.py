54611c0b625261b40d037e15800113c5f93fccbb
lbyrne8 <byrnelo@engr.oregonstate.edu>
2025-03-10 21:09:25 -0400

    edit to make it run

--- Holiday.py
+++ Holiday.py
@@ -5,6 +5,7 @@
 # https://pypi.org/project/holidays/
 # https://www.geeksforgeeks.org/python-holidays-library/
 # https://stackoverflow.com/questions/70391399/datetime-datatime-strptimedate-y-m-d-adds-000000-for-different-dates
+# https://www.geeksforgeeks.org/python-append-to-a-file/
 
 import time
 import holidays
@@ -15,28 +16,44 @@ us_holidays = holidays.US()
 
 if __name__ == '__main__':
     time.sleep(1)
+    # place to store all the dates as a list
+    added_dates = []
 
     # opens the personal calendar to check the date
     # sees if the date is in the right format
     # if not, an error text will pop up
-    with open("personal-calendar.txt", "r") as file:
-        line = file.readline().strip()
+    while True:
 
-    try:
-        event_date = datetime.datetime.strptime(line, "%Y-%m-%d").date()
+        with open("personal-calendar.txt", "r") as file:
+            dates = file.readlines()
+        # Checks all lines in the calendar for dates
+        # opens the holiday text file as append, to add new dates
+            with open("holiday-note.txt", "a") as file:
+                for line in dates:
+                    line = line.strip()
+                    if line not in added_dates:
+                        try:
+                            event_date = datetime.datetime.strptime(
+                                line, "%Y-%m-%d").date()
 
-        # If event is in the holiday package,
-        # then it will write onto the holiday-note.txt "Yes" and day what holiday it is
-        # if the event is not, it will write into the text "No" the date is not a holiday
-        if event_date in us_holidays:
+                            # If event is in the holiday package,
+                            # then write "Yes" in holiday-note.txt w/ holiday
+                            # then write "no" into holiday-note.txt w/ holiday
+                            # adds the dates to the previously checked dates
+                            # checks every 30 seconds
+                            if event_date in us_holidays:
+                                print(
+                                    f"{event_date} is a holiday: {us_holidays[event_date]}")
+                                result = print(
+                                    f"{event_date} is a holiday: {us_holidays[event_date]}")
+                                added_dates.append(result)
 
-            print(f"{event_date} is a holiday: {us_holidays[event_date]}")
-            with open("holiday-note.txt", "w") as file:
-                file.write(
-                    f"Yes\n{event_date} is a holiday: {us_holidays[event_date]}")
-        else:
-            print(f"{event_date} is not a holiday")
-            with open("holiday-note.txt", "w") as file:
-                file.write(f"{event_date} is not a holiday")
-    except ValueError:
-        print("Invalid date format in file. Expected YYYY-MM-DD.")
+                            else:
+                                print(f"{event_date} is not a holiday")
+                                result = f"{event_date} is not a holiday"
+                                added_dates.append(result)
+                        except ValueError:
+                            print(
+                                "Invalid date format in file. Expected YYYY-MM-DD.")
+            # cycle every 30 seconds
+            time.sleep(30)
